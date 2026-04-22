f07424b22da5d8d9fade7a90d28c755401d267fa
Hatinova <adela.hatinova@odbornaskola.cz>
2026-04-22 14:37:21 +0200

    naoujení na databázy ale moc to nefunguje

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+﻿from flask import Flask, render_template
+import sqlite3
+import os
 
 app = Flask(__name__)
 
+def get_db():
+    """Otevře spojení na databázi ptaci.db a nastaví row_factory."""
+    db = sqlite3.connect('ptaci.db')
+    db.row_factory = sqlite3.Row
+    return db
+
 @app.route('/')
 def dashboard():
-    return 'blblblbllb!'
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM ptaci ORDER BY nazev ASC')
+    ptaci = cursor.fetchall()
+    db.close()
+    return render_template('dashboard.html', ptaci=ptaci)
 
 if __name__ == '__main__':
     app.run(debug=True)
